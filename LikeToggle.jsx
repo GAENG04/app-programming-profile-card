f2b024fa84549746cb89724934a53bc4bfbe0547
GAENG04 <mim49123@gmail.com>
2025-09-10 11:34:21 +0900

    components

---
+++ LikeToggle.jsx
@@ -0,0 +1,24 @@
+import
+{useState}
+from
+"react"
+;
+export default function
+LikeToggle
+() {
+
+    const [liked, setLiked] = useState(
+        false
+    );
+    
+    return
+    (
+        <button onClick={() => setLiked(v => !v)}>
+            {liked ?
+            "★ Liked"
+        :
+        "☆ Like"
+        }
+        </button>
+    );
+}
